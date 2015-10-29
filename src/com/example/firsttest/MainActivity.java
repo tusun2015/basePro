32bf3ae40e3979613408fbc2e6bcf2ee97879304
wenhai <wenhai@wenhai-PC>
2015-10-29 11:06:31 +0800

    sfsFS

--- src/com/example/firsttest/MainActivity.java
+++ src/com/example/firsttest/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	private int x;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
